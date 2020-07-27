ba61c87980af5c813ddbb93f5446c46821d65b16
Henrique Rezende <rezendech@gmail.com>
2020-07-27 14:53:54 -0300

    Adicionando classe que manipula mensagens para o destino /hello

---
+++ src/main/java/com/example/messagingstompwebsocket/GreetingController.java
@@ -0,0 +1,19 @@
+package com.example.messagingstompwebsocket;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+//Class to handle messages to the /hello destination
+@Controller
+public class GreetingController {
+
+    @MessageMapping("/hello")
+    @SendTo("/topics/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+    }
+
+}
